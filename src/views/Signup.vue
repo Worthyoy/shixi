117ba1da49136c24470bc75165194d336149bab9
小怡酱c <879373096@qq.com>
2023-06-13 18:01:06 +0800

    signup

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,26 +1,33 @@
 <template>
     <el-form class="login" :model="form" label-width="120px">
-        <h2>Please sign in</h2>
-        <el-form-item label="账号">
-            <el-input v-model="form.name" />
+        <h2>Please sign up</h2>
+        <el-form-item label="email">
+            <el-input v-model="form.email" />
         </el-form-item>
-        <el-form-item label="密码">
-            <el-input v-model="form.region" />
+        <el-form-item label="mobile">
+            <el-input v-model="form.mobile" />
         </el-form-item>
-        <!-- 验证码 -->
-        <el-form-item label="验证码" class="s">
-            <el-input v-model="form.date1" style="width: 418px;"/>
-            <!-- 点击可变的验证码图片 -->
-            <img width="60" height="30" src="https://img0.baidu.com/it/u=2673243120,3070690751&fm=253&fmt=auto&app=138&f=JPEG?w=417&h=211" alt="">
+        <el-form-item label="telephone">
+            <el-input v-model="form.telephone" />
         </el-form-item>
-        <el-form-item id="btn_box">
-            <el-button style="width: 480px;" type="primary" @click="onSubmit">登录</el-button>
+        <el-form-item label="wechat_num">
+            <el-input v-model="form.wechat_num" />
         </el-form-item>
-        <el-form-item id="btn_box">
-            <el-button style="width: 480px;" type="primary" @click="signup">注册</el-button>
+        <el-form-item label="telecom_num">
+            <el-input v-model="form.telecom_num" />
         </el-form-item>
-        
-
+        <el-form-item label="username">
+            <el-input v-model="form.username" />
+        </el-form-item>
+        <el-form-item label="password">
+            <el-input v-model="form.password" />
+        </el-form-item>
+        <el-form-item label="sysrole">
+            <el-input v-model="form.sysrole" />
+        </el-form-item>
+        <el-form-item>
+            <el-button style="width: 480px;" type="primary" @click="onSubmit">注册</el-button>
+        </el-form-item>
     </el-form>
 </template>
 <script setup>
@@ -35,23 +42,22 @@ import { ElMessage } from 'element-plus'
 //使用路由
 const router = useRouter()
 const form = ref({
-    name: 'xxx@xxx',
-    region: '123',
-    date1: '',
-    date2: '',
-    delivery: false,
-    type: [],
-    resource: '',
-    desc: ''
+    email: "3",
+    mobile: "3",
+    telephone: "3",
+    wechat_num: "3",
+    telecom_num: "3",
+    username: "3334",
+    password: "3",
+    sysrole: "3",
+    orgid: 0
 })
 const onSubmit = () => {
-    axios.post('/api/auth/login', {
-        email:form.value.name,
-        password:form.value.region,
-    }).then((rsp) => {
+    axios.post('/api/user/', form.value).then((rsp) => {
         console.log(rsp.data);
         if (rsp.data.status == 'success') {
-            router.push('/home')
+            router.push('/')
+            ElMessage('Signup success.');
         } else {
             ElMessage('this is a message.');//提示消息框或按钮。将rsp.data传递给页面中的
         }
@@ -61,20 +67,17 @@ const onSubmit = () => {
     })
     
 }
-function signup() {
-    router.push('/signup')
-}
 
 </script>
 <style scoped>
     .el-form{
         position: absolute;
         left: 50%;
-        top: 50%;
+        top: 37%;
         margin-left: -300px;
         margin-top: -150px;
         width: 600px;
-        height: 300px;
+        height: 500px;
         border: 1px solid #ccc;
         padding: 10px 40px 0 0;
     }
@@ -86,8 +89,5 @@ function signup() {
         font-size: 30px;
         text-align: center;
         font-weight: bolder;
-    }
-    #btn_box .el-form-item__content {
-        flex-wrap: nowrap !important;
     }
 </style>
